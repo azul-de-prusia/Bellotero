0fc1d5f4fc421b9ccaeb3412249caf433d642954
joasimar <joasimar@hotmail.com>
2020-09-20 13:57:08 -0500

    Updates

---
+++ precache-manifest.f469357df1f966389c0add8c99177836.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d97ae7ad7f23e5349ca7280f06287b",
+    "url": "/Bellotero/index.html"
+  },
+  {
+    "revision": "ab7892421886cfcd4dfb",
+    "url": "/Bellotero/static/css/main.dc977176.chunk.css"
+  },
+  {
+    "revision": "866d02789cf97deb3f73",
+    "url": "/Bellotero/static/js/2.a64c61df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Bellotero/static/js/2.a64c61df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab7892421886cfcd4dfb",
+    "url": "/Bellotero/static/js/main.e4f581b8.chunk.js"
+  },
+  {
+    "revision": "ce8d6fd53788644a3c22",
+    "url": "/Bellotero/static/js/runtime-main.4dfa7dd8.js"
+  }
+]);
